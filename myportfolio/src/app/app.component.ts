3793427a2a8322f7da631a3213ea2cd3c6798b14
seni0rDEV <132949016+seni0rDEV@users.noreply.github.com>
2024-11-28 16:47:41 +0100

    working on the navbar

--- myportfolio/src/app/app.component.ts
+++ myportfolio/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from './navbar/navbar.component';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'myportfolio';
+  title = 'des';
 }
