69fe102efab09292472ac44591ac7b70d4d69d39
seni0rDEV <132949016+seni0rDEV@users.noreply.github.com>
2024-12-13 11:55:42 +0100

    install bootstrap 5

--- myportfolio/src/app/app.component.ts
+++ myportfolio/src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from './navbar/navbar.component';
-import { RouterOutlet } from '@angular/router';
+// import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [ NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
